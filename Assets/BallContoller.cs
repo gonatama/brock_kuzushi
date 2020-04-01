5bb23e1e2518e1d2241cc970b2a887f781bbf3d7
gon_fox <try.to.avoid@gmail.com>
2020-04-01 18:53:01 +0900

    球に初速を与えるスクリプトを作成

--- Assets/BallContoller.cs
+++ Assets/BallContoller.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class BallContoller : MonoBehaviour
 {
+    float Speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody>().AddForce((transform.forward + transform.right) * Speed, ForceMode.VelocityChange);
     }
 
     // Update is called once per frame
