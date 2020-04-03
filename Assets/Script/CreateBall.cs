7e25dc264ae8641de25ea20b772c0c6698912f8c
gon_fox <try.to.avoid@gmail.com>
2020-04-03 11:05:35 +0900

    新しく球を生成する機能を追加（未完成） ＢＧＭをループするように変更

---
+++ Assets/Script/CreateBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = (GameObject)Resources.Load("Prefab/Ball");
+
+        Instantiate(obj, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+
+            GameObject obj = (GameObject)Resources.Load("Prefab/Ball");
+
+            Instantiate(obj, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        }
+    }
+}
