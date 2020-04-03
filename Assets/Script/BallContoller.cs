6e220c6380e4c149a936aa5a6832abd2dab585cf
gon_fox <try.to.avoid@gmail.com>
2020-04-03 14:40:40 +0900

    ボールの動きを垂直・水平に動かないように変更。

--- Assets/Script/BallContoller.cs
+++ Assets/Script/BallContoller.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class BallContoller : MonoBehaviour
 {
-    float Speed = 10.0f;
+    float Speed = 15.0f;
+    //int up = 0;
+    private Rigidbody ball;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce((transform.forward + transform.right) * Speed, ForceMode.VelocityChange);
+        ball = GetComponent<Rigidbody>();
+        Debug.Log(ball);
+        ball.AddForce((transform.forward + transform.right) * Speed, ForceMode.VelocityChange);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Mathf.Abs(ball.velocity.z) < 3f)
+        {
+            ball.velocity = new Vector3(ball.velocity.x, ball.velocity.y, ball.velocity.z * 3f);
+        }
+
+        if (Mathf.Abs(ball.velocity.x) < 3f)
+        {
+            ball.velocity = new Vector3(ball.velocity.x * 3f, ball.velocity.y, ball.velocity.z);
+        }
+
     }
 }
